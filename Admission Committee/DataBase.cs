f5dc67a9af01544f0757c5023f76c82eb8d4409a
KinExp <kinexp.08@yandex.ru>
2024-04-29 21:22:06 +0500

    Добавьте файлы проекта.

---
+++ Admission Committee/DataBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Admission_Committee
+{
+    internal class DataBase
+    {
+        SqlConnection sqlConnection = new SqlConnection(@"Data Source=KINEXP;Initial Catalog=ПриёмнаяКомиссия;Integrated Security=True");
+        //SqlConnection sqlConnection = new SqlConnection(@"Data Source=KINEXP;Initial Catalog=ПриёмнаяКомиссия;Integrated Security=True");
+        //SqlConnection sqlConnection = new SqlConnection(@"Data Source=KINEXP;Initial Catalog=ПриёмнаяКомиссия;Integrated Security=True");
+
+        public void OpenConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Closed)
+                sqlConnection.Open();
+        }
+
+        public void CloseConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Open)
+                sqlConnection.Close();
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return sqlConnection;
+        }
+    }
+}

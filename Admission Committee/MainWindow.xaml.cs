88988390e6a11bd9dc51f77ecaff549cbb4522ee
KinExp <kinexp.08@yandex.ru>
2024-05-19 13:45:04 +0500

    Complite method RefreshDataGrid, Add window movement, Add an incomplete exit check
    Co-Authored-By: Evgeniy <147424002+sssjka@users.noreply.github.com>

--- Admission Committee/MainWindow.xaml.cs
+++ Admission Committee/MainWindow.xaml.cs
@@ -20,40 +20,79 @@ namespace Admission_Committee
     public partial class MainWindow : Window
     {
         DataBase dataBase;
-        DataSet dataSet;
-        List<string> tables;
+        bool exit;
+
         public MainWindow()
         {
             InitializeComponent();
 
             dataBase = new DataBase();
-            dataSet = new DataSet();
-            tables = new List<string>();
-            tables.Add("Абитуриенты");
+            exit = false;
 
-            //RefreshDataGrid(applicantsDataGrid, tables[0]);
-
+            RefreshDataGrid();
         }
 
-        private void RefreshDataGrid(DataGrid dg, string table)
+        private void RefreshDataGrid()
         {
-            string sql = "SELECT Серия_паспорта, Номер_паспорта, Фамилия, Имя, Отчество, Пол FROM " + table;
+            string sqlQueryApplicents = "SELECT Серия_паспорта, Номер_паспорта, Фамилия, Имя, Отчество, Пол, Дата_рождения, " +
+                                        "Телефон, Название_учебного_заведения, Год_окончания_учебного_заведения, Средний_балл_аттестата FROM Абитуриенты Аб " +
+                                        "INNER JOIN Аттестаты Ат ON Ат.Серия_аттестата = Аб.Серия_аттестата AND Ат.Номер_аттестата = Аб.Номер_аттестата";
+            string sqlQueryStatements = "SELECT Код_заявления, Фамилия, Имя, Отчество, Название_специальности, З.Уровень_образования, " +
+                                        "З.Вариант_обучения, З.Форма_обучения FROM Заявления_на_поступление З " +
+                                        "INNER JOIN Абитуриенты А ON А.Серия_паспорта = З.Серия_паспорта AND А.Номер_паспорта = З.Номер_паспорта " +
+                                        "INNER JOIN Специальности С ON С.Код_специальности = З.Код_специальности AND С.Уровень_образования = З.Уровень_образования " +
+                                        "AND С.Вариант_обучения = З.Вариант_обучения AND С.Форма_обучения = З.Форма_обучения";
+            string sqlQuerySpecialties = "SELECT * FROM Специальности";
 
             dataBase.OpenConnection();
 
-            SqlDataAdapter adapter = new SqlDataAdapter(sql, dataBase.GetConnection());
+            SqlDataAdapter adapterApplicents = new SqlDataAdapter(sqlQueryApplicents, dataBase.GetConnection());
+            SqlDataAdapter adapterStatements = new SqlDataAdapter(sqlQueryStatements, dataBase.GetConnection());
+            SqlDataAdapter adapterSpecialties = new SqlDataAdapter(sqlQuerySpecialties, dataBase.GetConnection());
 
-            adapter.Fill(dataSet, table);
+            DataSet dataSet = new DataSet();
+            dataSet.Tables.Add();
+            dataSet.Tables.Add();
+            dataSet.Tables.Add();
+
+            adapterApplicents.Fill(dataSet.Tables[0]);
+            adapterStatements.Fill(dataSet.Tables[1]);
+            adapterSpecialties.Fill(dataSet.Tables[2]);
             
-            DataTable dataTableOrders = new DataTable("Абитуриенты");
-            dataTableOrders.Columns.Add("PassportSeries");
-            dataTableOrders.Columns.Add("PassportNumber");
-            dataTableOrders.Columns.Add("SecondName");
-            dataTableOrders.Columns.Add("FirstName");
-            dataTableOrders.Columns.Add("ThirdName");
-            dataTableOrders.Columns.Add("Gender");
+            DataTable dtApplicents = new DataTable("Абитуриенты");
+            dtApplicents.Columns.Add("PassportSeries");
+            dtApplicents.Columns.Add("PassportNumber");
+            dtApplicents.Columns.Add("SecondName");
+            dtApplicents.Columns.Add("FirstName");
+            dtApplicents.Columns.Add("ThirdName");
+            dtApplicents.Columns.Add("Gender");
+            dtApplicents.Columns.Add("Birthday");
+            dtApplicents.Columns.Add("Phone");
+            dtApplicents.Columns.Add("School");
+            dtApplicents.Columns.Add("YearGraduation");
+            dtApplicents.Columns.Add("AvarageScore");
 
-            foreach (DataRow row in dataSet.Tables[table].Rows)
+            DataTable dtStatements = new DataTable("Заявления");
+            dtStatements.Columns.Add("ID");
+            dtStatements.Columns.Add("SecondName");
+            dtStatements.Columns.Add("FirstName");
+            dtStatements.Columns.Add("ThirdName");
+            dtStatements.Columns.Add("NameSpecialty");
+            dtStatements.Columns.Add("Level");
+            dtStatements.Columns.Add("Option");
+            dtStatements.Columns.Add("Form");
+
+            DataTable dtSpecialties = new DataTable("Специальности");
+            dtSpecialties.Columns.Add("ID");
+            dtSpecialties.Columns.Add("Level");
+            dtSpecialties.Columns.Add("NameSpecialty");
+            dtSpecialties.Columns.Add("Option");
+            dtSpecialties.Columns.Add("Form");
+            dtSpecialties.Columns.Add("AmountPlaces");
+            dtSpecialties.Columns.Add("Cost");
+            dtSpecialties.Columns.Add("Time");
+
+            foreach (DataRow row in dataSet.Tables[0].Rows)
             {
                 List<string> items = new List<string>();
 
@@ -66,23 +105,69 @@ namespace Admission_Committee
                 else
                     items[5] = "М";
 
-                DataRow newRow = dataTableOrders.NewRow();
+                items[6] = items[6].Substring(0, 10);
+
+                DataRow newRow = dtApplicents.NewRow();
                 newRow.ItemArray = items.ToArray();
-                dataTableOrders.Rows.Add(newRow);
+                dtApplicents.Rows.Add(newRow);
             }
 
-            dg.ItemsSource = dataTableOrders.DefaultView;
+            foreach(DataRow row in dataSet.Tables[1].Rows)
+            {
+                List<string> items = new List<string>();
+
+                var cells = row.ItemArray;
+                foreach (object cell in cells)
+                    items.Add(cell.ToString());
+
+                DataRow newRow = dtStatements.NewRow();
+                newRow.ItemArray = items.ToArray();
+                dtStatements.Rows.Add(newRow);
+            }
+
+            foreach(DataRow row in dataSet.Tables[2].Rows)
+            {
+                List<string> items = new List<string>();
+
+                var cells = row.ItemArray;
+                foreach (object cell in cells)
+                    items.Add(cell.ToString());
+
+                DataRow newRow = dtSpecialties.NewRow();
+                newRow.ItemArray = items.ToArray();
+                dtSpecialties.Rows.Add(newRow);
+            }
+
+            applicantsDataGrid.ItemsSource = dtApplicents.DefaultView;
+            statementsDataGrid.ItemsSource = dtStatements.DefaultView;
+            specialtiesDataGrid.ItemsSource = dtSpecialties.DefaultView;
+        }
+
+        private void RefreshImage_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            RefreshDataGrid();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            ApplicentsDetailsWindow aplWin = new ApplicentsDetailsWindow();
+            aplWin.ShowDialog();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void CloseImage_MouseLeftButtonDown (object sender, MouseButtonEventArgs e)
+        {
+            exit = true;
         }
 
         private void CloseImage_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            Close();
-        }
-
-        private void addButton_Click(object sender, RoutedEventArgs e)
-        {
-            ApplicentsDetailsWindow aplWin = new ApplicentsDetailsWindow();
-            aplWin.ShowDialog();
+            if (exit)
+                Close();
         }
     }
 }
